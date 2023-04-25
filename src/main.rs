8a8b7a8d6b1daa3c1f8a13f9b39b8c09f645f3ef
psysa14 <psysa14@nottingham.ac.uk>
2023-04-25 12:03:48 +0100

    Spawn meteors, crashes on collision though :(

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 
 use std::f32::consts::PI;
 
+use rand::prelude::*;
 use raylib::consts::KeyboardKey::*;
 use raylib::ffi::{GetFrameTime, GetWorldToScreen2D};
 use raylib::prelude::*;
@@ -18,6 +19,9 @@ struct Game {
     timer: f32,
     background: Color,
     camera: Camera2D,
+    rand: ThreadRng,
+    meteorTexture: Image,
+    meteorSpawn: f32,
 }
 
 struct Player {
@@ -81,8 +85,6 @@ impl Player {
             self.rotation + 90.0,
             self.tint,
         );
-
-        d.draw_circle_v(pos, 10.0, Color::GREEN);
     }
 
     fn update(&mut self, rl: &RaylibHandle) {
@@ -149,8 +151,13 @@ impl Ball {
             self.rotation + 90.0,
             self.tint,
         );
+    }
 
-        d.draw_circle_v(pos, 20.0, Color::RED);
+    fn update(&mut self) {
+        self.position += Vector2::new(
+            self.speed * (self.rotation * PI / 180.0).cos(),
+            self.speed * (self.rotation * PI / 180.0).sin(),
+        );
     }
 }
 
@@ -189,26 +196,36 @@ fn main() {
             target: Vector2::new(0.0, 0.0),
             zoom: 1.0,
         },
+        rand: rand::thread_rng(),
+        meteorTexture: asteroid,
+        meteorSpawn: 1.0,
     };
 
     game.meteors.push(Ball {
         position: Vector2::new(SCREEN_WIDTH / 2.0, SCREEN_HEIGHT / 2.0),
-        speed: 20.0,
+        speed: 0.0,
         rotation: 45.0,
-        tint: Color::WHITE,
-        texture: rl.load_texture_from_image(&thread, &asteroid).unwrap(),
+        tint: Color::GREEN,
+        texture: rl
+            .load_texture_from_image(&thread, &game.meteorTexture)
+            .unwrap(),
     });
 
     game.meteors.push(Ball {
         position: Vector2::new(150.0, 150.0),
-        speed: 3.0,
+        speed: 0.0,
         rotation: 20.0,
-        tint: Color::BROWN,
-        texture: rl.load_texture_from_image(&thread, &asteroid).unwrap(),
+        tint: Color::GREEN,
+        texture: rl
+            .load_texture_from_image(&thread, &game.meteorTexture)
+            .unwrap(),
     });
 
     // Main loop
     while !rl.window_should_close() {
+        // Spawn asteroids
+        spawnAsteroids(&mut rl, &mut game, &thread);
+
         // Handle movement and check for collisions
         mainLoop(&rl, &mut game);
 
@@ -217,9 +234,44 @@ fn main() {
     }
 }
 
+fn spawnAsteroids(rl: &mut RaylibHandle, game: &mut Game, thread: &RaylibThread) {
+    // Check if meteor should spawn
+    if game.meteorSpawn <= 0.0 {
+        // Generate direction of spawn from player
+        let direction: i32 = game.rand.gen_range(0..361);
+
+        // Generate spawn position of meteor
+        let pos = Vector2::new(
+            game.player.position.x + 250.0 * (direction as f32 * PI / 180.0).cos(),
+            game.player.position.y + 250.0 * (direction as f32 * PI / 180.0).sin(),
+        );
+
+        // Instantiate meteor
+        game.meteors.push(Ball {
+            position: pos,
+            speed: game.rand.gen_range(2..5) as f32,
+            rotation: 360.0 - direction as f32 + game.rand.gen_range(-20..20) as f32,
+            tint: Color::BROWN,
+            texture: rl
+                .load_texture_from_image(&thread, &game.meteorTexture)
+                .unwrap(),
+        });
+
+        // Reset meteor spawn
+        game.meteorSpawn = 1.0;
+    } else {
+        unsafe { game.meteorSpawn = game.meteorSpawn - GetFrameTime() };
+    }
+}
+
 fn mainLoop(rl: &RaylibHandle, game: &mut Game) {
     // Update player
     game.player.update(rl);
+
+    // Update meteors
+    for i in 0..game.meteors.len() {
+        game.meteors[i].update();
+    }
 
     // Check for player meteor collisions
     for i in 0..game.meteors.len() {
@@ -242,18 +294,18 @@ fn mainLoop(rl: &RaylibHandle, game: &mut Game) {
 }
 
 fn drawGame(rl: &mut RaylibHandle, game: &Game, thread: &RaylibThread) {
-        // Draw background
-        let mut d = rl.begin_drawing(&thread);
-        d.clear_background(game.background);
+    // Draw background
+    let mut d = rl.begin_drawing(&thread);
+    d.clear_background(game.background);
 
-        // Draw player
-        game.player.draw(&mut d, game.camera);
+    // Draw player
+    game.player.draw(&mut d, game.camera);
 
-        // Draw meteors
-        for x in &game.meteors {
-            x.draw(&mut d, game.camera);
-        }
+    // Draw meteors
+    for x in &game.meteors {
+        x.draw(&mut d, game.camera);
+    }
 
-        // Draw timer
-        d.draw_text(&(game.timer as i32).to_string(), 10, 10, 40, Color::WHITE);
+    // Draw timer
+    d.draw_text(&(game.timer as i32).to_string(), 10, 10, 40, Color::WHITE);
 }
